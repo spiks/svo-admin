0ab6ea9ec947c1aedb4473fd538cff327783ff5d
Viktor Skorodumov <skorodumoviktor@gmail.com>
2022-02-07 19:56:47 +0300

    fix: lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ require("../styles/ant.less");
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
