9a74cbcf49e4f6c2ffd2753f7dae871da3310572
sasha💜 <zaitseva@spiks.ru>
2022-08-09 16:51:33 +0300

    refactor: sidebar position

--- components/MainLayout/MainLayout.component.tsx
+++ components/MainLayout/MainLayout.component.tsx
@@ -1,16 +1,31 @@
 import { FC } from 'react';
 import { Layout } from 'antd';
 import { SiderMenu } from '../SiderMenu/SiderMenu.component';
-
 const { Sider, Content } = Layout;
 
 export const MainLayout: FC = ({ children }) => {
   return (
-    <Layout>
-      <Sider width={280}>
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+        width={280}
+      >
         <SiderMenu />
       </Sider>
-      <Content style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>{children}</Content>
+      <Layout
+        style={{
+          marginLeft: 280,
+        }}
+      >
+        <Content style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>{children}</Content>
+      </Layout>
     </Layout>
   );
 };
