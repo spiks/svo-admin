c2d59e05c6dc7fb8947a1701a6e27c3a2aeec1d3
Matveev Ivan <84996375+igorevichfromspb@users.noreply.github.com>
2022-08-09 17:37:09 +0300

    feat: added Breadcrumbs component

---
+++ components/Breadcrumbs/breadcrumbsConfig.ts
@@ -0,0 +1,9 @@
+export const breadCrumbsConfig: Record<string, string> = {
+  users: 'Пользователи',
+  patients: 'Клиенты',
+  therapists: 'Психологи',
+  moderators: 'Модераторы',
+  supportAgents: 'Агенты поддержки',
+  administrators: 'Администраторы',
+  blog: 'Блог',
+};
