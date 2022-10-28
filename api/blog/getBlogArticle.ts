662761aa0f372574252dca3483a94c7276c40bd6
sasha💜 <zaitseva@spiks.ru>
2022-10-28 14:00:58 +0300

    feat: delete mocks

--- api/blog/getBlogArticle.ts
+++ api/blog/getBlogArticle.ts
@@ -1,6 +1,5 @@
 import { AdminBlogServiceWithToken } from 'api/services';
 import { Uuid } from 'generated';
-import { mediaImageMock } from '../mocks';
 
 export const getBlogArticle = async (id: Uuid) => {
   return AdminBlogServiceWithToken.getBlogArticle({ requestBody: { arguments: { id } } });
