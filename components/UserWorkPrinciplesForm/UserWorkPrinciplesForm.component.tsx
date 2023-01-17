5efd19a638b9305717edad38d62becc93014284b
Matveev Ivan <84996375+igorevichfromspb@users.noreply.github.com>
2023-01-17 14:43:25 +0300

    feat: added therapist work principles tab

---
+++ components/UserWorkPrinciplesForm/UserWorkPrinciplesForm.component.tsx
@@ -0,0 +1,61 @@
+import { Button, Form, FormProps, notification } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
+import { updateTherapistWorkPrinciples } from 'api/therapist/updateTherapistWorkPrinciples';
+import { LongDescription } from 'generated';
+import { TherapistPageContext } from 'pages/users/therapists/[id]';
+import { FC, useContext } from 'react';
+
+export const UserWorkPrinciplesForm: FC = () => {
+  const [form] = Form.useForm();
+  const { therapist } = useContext(TherapistPageContext);
+
+  const onFinish: FormProps<{ workPrinciples: LongDescription }>['onFinish'] = async (values) => {
+    try {
+      await updateTherapistWorkPrinciples({ ...values, therapistId: therapist.id });
+      notification.success({
+        type: 'success',
+        message: 'Успех',
+        description: 'Информация сохранена',
+      });
+    } catch (e) {
+      notification.error({
+        type: 'error',
+        message: 'Ошибка',
+        description: 'Не удалось сохранить информацию',
+      });
+    }
+  };
+
+  return (
+    <div style={{ padding: '80px 160px' }}>
+      <Form
+        onFinish={onFinish}
+        form={form}
+        size="large"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{
+          workPrinciples: therapist.workPrinciples,
+        }}
+      >
+        <Form.Item
+          normalize={(value) => {
+            if (!value) {
+              return null;
+            }
+            return value;
+          }}
+          label="Подход и принципы работы"
+          name="workPrinciples"
+        >
+          <TextArea rows={6} showCount maxLength={2048} />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: '8' }}>
+          <Button type="primary" htmlType="submit">
+            Сохранить
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
