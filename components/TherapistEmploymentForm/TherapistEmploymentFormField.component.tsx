81f66adbb94f1bce2bb58b043c5e6e9d68f7bbdd
Mikhail Kalinin <kalinin@spiks.ru>
2023-01-27 20:17:39 +0300

    fix(therapist): limited work expreience up to 30 years

--- components/TherapistEmploymentForm/TherapistEmploymentFormField.component.tsx
+++ components/TherapistEmploymentForm/TherapistEmploymentFormField.component.tsx
@@ -9,15 +9,42 @@ type Props = {
 
 export const TherapistEmploymentFormField: FC<Props> = ({ name, onDeleteButtonClick }) => {
   return (
-    <Row gutter={[16, 0]} align={'bottom'}>
+    <Row gutter={[16, 0]} align={'top'}>
       <Col flex={'1'}>
-        <Form.Item style={{ margin: 0 }} label="Практический опыт (лет)" name={[name, 'years']}>
-          <Input type={'number'} max={100} />
+        <Form.Item
+          style={{ margin: 0 }}
+          label="Практический опыт (лет)"
+          rules={[
+            {
+              message: 'Обязательно укажите опыт',
+              required: true,
+            },
+            {
+              async validator(_, v) {
+                if (Number(v) && (v < 1 || v > 30)) {
+                  throw new Error('Значение от 1 до 30 лет');
+                }
+              },
+            },
+          ]}
+          name={[name, 'years']}
+        >
+          <Input type={'number'} />
         </Form.Item>
       </Col>
       <Col flex={'1'}>
-        <Form.Item style={{ margin: 0 }} label="Место работы" name={[name, 'companyName']}>
-          <Input required />
+        <Form.Item
+          style={{ margin: 0 }}
+          label="Место работы"
+          name={[name, 'companyName']}
+          rules={[
+            {
+              required: true,
+              message: 'Укажите название места работы',
+            },
+          ]}
+        >
+          <Input />
         </Form.Item>
       </Col>
       <Col>
