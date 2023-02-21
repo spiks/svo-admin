788b3aef7e3b715a87fb6595cd1602b3d44203b7
Matveev Ivan <84996375+igorevichfromspb@users.noreply.github.com>
2023-02-21 19:05:37 +0300

    fix: #389

--- components/TherapistDocumentsForm/TherapistDocumentsForm.documents/PassportForm/PassportForm.rules/byNumber.rule.ts
+++ components/TherapistDocumentsForm/TherapistDocumentsForm.documents/PassportForm/PassportForm.rules/byNumber.rule.ts
@@ -2,7 +2,7 @@ import { Rule } from 'rc-field-form/es/interface';
 
 export const byNumber: Rule = {
   type: 'string',
-  len: 8,
-  pattern: /^[A-Z]{2}[0-9]{6}$/,
-  message: 'Введите корректное значение (AZ000000)',
+  len: 9,
+  pattern: /^[A-Z]{2}[0-9]{7}$/,
+  message: 'Введите корректное значение (AZ0000000)',
 };
