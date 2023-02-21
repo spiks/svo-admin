94a557309ed00893ccb3ef7f2403ca169a6c8cee
Matveev Ivan <84996375+igorevichfromspb@users.noreply.github.com>
2023-02-21 18:56:09 +0300

    fix: #388

--- components/TherapistDocumentsForm/TherapistDocumentsForm.documents/PassportForm/PassportForm.rules/type.rule.ts
+++ components/TherapistDocumentsForm/TherapistDocumentsForm.documents/PassportForm/PassportForm.rules/type.rule.ts
@@ -2,7 +2,7 @@ import { Rule } from 'rc-field-form/es/interface';
 
 export const type: Rule = {
   type: 'string',
-  len: 2,
-  pattern: /^[A-Z]{2}$/,
-  message: 'Введите корректное значение (AA)',
+  max: 2,
+  pattern: /^[A-Z]{1,2}$/,
+  message: 'Введите корректное значение (A/AA)',
 };
