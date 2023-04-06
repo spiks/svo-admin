dc2dc415027a14c84ccde68f0774fe40813122db
sasha💜 <zaitseva@spiks.ru>
2023-04-06 13:42:44 +0300

    feat: add routing for blog articles tabs

--- pages/content/blog/index.tsx
+++ pages/content/blog/index.tsx
@@ -7,6 +7,7 @@ import { BlogHeader } from '@components/BlogHeader/BlogHeader.component';
 import { MainLayout } from '@components/MainLayout/MainLayout.component';
 import { PageWrapper } from '@components/PageWrapper/PageWrapper.component';
 import { ArticleBlogStatus as ApiArticleBlogStatus } from '../../../generated';
+import { useRouter } from 'next/router';
 
 const BlogListComponent = dynamic(() => import('@components/BlogList/BlogList.component'), {
   loading: () => <SplashScreenLoader />,
@@ -14,7 +15,17 @@ const BlogListComponent = dynamic(() => import('@components/BlogList/BlogList.co
 
 export type ArticleBlogStatus = ApiArticleBlogStatus | 'article_archived';
 
-const tabListItems: { label: string; key: ArticleBlogStatus | 'article_archived' }[] = [
+function isArticleBlogStatusType(value: string): value is ArticleBlogStatus {
+  const statuses: ArticleBlogStatus[] = [
+    'article_awaiting_review',
+    'article_published',
+    'article_rejected',
+    'article_archived',
+  ];
+  return statuses.includes(value as never);
+}
+
+const tabListItems: { label: string; key: ArticleBlogStatus }[] = [
   { label: 'Опубликованные', key: 'article_published' },
   { label: 'Архивные', key: 'article_archived' },
   { label: 'На модерации', key: 'article_awaiting_review' },
@@ -23,11 +34,22 @@ const tabListItems: { label: string; key: ArticleBlogStatus | 'article_archived'
 
 const BlogPage: NextPage = () => {
   const [showFilters, setShowFilters] = useState<boolean>(false);
-  const [activeTab, setActiveTab] = useState<ArticleBlogStatus>('article_published');
+  const router = useRouter();
 
-  const handleTabListChange = useCallback((key) => {
-    setActiveTab(key);
-  }, []);
+  const handleTabListChange = useCallback(
+    (key) => {
+      router.push({
+        query: {
+          ...router.query,
+          activeTab: key,
+        },
+      });
+    },
+    [router],
+  );
+
+  const queryActiveTab = router.query.activeTab as ArticleBlogStatus;
+  const activeTab = isArticleBlogStatusType(queryActiveTab) ? queryActiveTab : 'article_published';
 
   const handleShowFilters = () => {
     setShowFilters(!showFilters);
@@ -36,7 +58,12 @@ const BlogPage: NextPage = () => {
   return (
     <MainLayout>
       <BlogHeader showFilters={showFilters} handleShowFilters={handleShowFilters}>
-        <TabList items={tabListItems} defaultActiveKey={'published'} onChange={handleTabListChange} />
+        <TabList
+          items={tabListItems}
+          defaultActiveKey={'published'}
+          activeKey={activeTab}
+          onChange={handleTabListChange}
+        />
       </BlogHeader>
       <div style={{ overflow: 'auto' }}>
         <PageWrapper>
