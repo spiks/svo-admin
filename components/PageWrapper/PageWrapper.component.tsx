bb72c60a420ad9576fe3f589b749d68f8bc030bd
Andrei Zotov <134272420+zotovprog@users.noreply.github.com>
2023-06-01 12:32:33 +0300

    fix: #531 (#573)
    Update PageWrapper styles, add scroll on all table
    
    Co-authored-by: Andrei Zotov <zotov@spiks.ru>

--- components/PageWrapper/PageWrapper.component.tsx
+++ components/PageWrapper/PageWrapper.component.tsx
@@ -1,6 +1,11 @@
 import Card from 'antd/lib/card/Card';
 import { FC } from 'react';
+import styles from './PageWrapper.module.css';
 
 export const PageWrapper: FC = ({ children }) => {
-  return <Card style={{ margin: '40px', borderRadius: '8px' }}>{children}</Card>;
+  return (
+    <Card style={{ margin: '40px', borderRadius: '8px' }} className={styles['page-wrapper']}>
+      {children}
+    </Card>
+  );
 };
