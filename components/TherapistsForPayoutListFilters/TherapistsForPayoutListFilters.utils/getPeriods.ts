68af2d161433f988449d5edbd6f1630de57c59f8
Matveev Ivan <84996375+igorevichfromspb@users.noreply.github.com>
2023-06-19 17:34:45 +0300

    fix: period dates

--- components/TherapistsForPayoutListFilters/TherapistsForPayoutListFilters.utils/getPeriods.ts
+++ components/TherapistsForPayoutListFilters/TherapistsForPayoutListFilters.utils/getPeriods.ts
@@ -3,11 +3,11 @@ import moment from 'moment';
 export const getPeriods = (date: moment.Moment) => {
   const currentMonth = moment(date).format('MM');
   const endOfPrevMonth = moment(date).subtract(1, 'months').endOf('months').format('DD.MM');
-  const endOfMonth = moment(date).endOf('months').format('DD.MM');
+  const penultimateDayOfMonth = moment(date).endOf('months').subtract(1, 'days').format('DD.MM');
 
-  const firstPeriod = `${endOfPrevMonth} - 9.${currentMonth}`;
-  const secondPeriod = `9.${currentMonth} - 19.${currentMonth}`;
-  const thirdPeriod = `19.${currentMonth} - ${endOfMonth}`;
+  const firstPeriod = `${endOfPrevMonth} - 8.${currentMonth}`;
+  const secondPeriod = `9.${currentMonth} - 18.${currentMonth}`;
+  const thirdPeriod = `19.${currentMonth} - ${penultimateDayOfMonth}`;
 
   return {
     firstPeriod,
